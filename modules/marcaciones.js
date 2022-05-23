cfd4ef531587c4c74302e798d9f26d31befd25e8
rodolfovenegas <rodolfovenegas.es@gmail.com>
2022-05-23 09:11:03 -0400

    Agrego logs con gfecha y hora de marcacion y click en boton para generar marcacion

--- modules/marcaciones.js
+++ modules/marcaciones.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer')
-/*Obtengo variables de entonrto utilizando dotenv: https://www.npmjs.com/package/dotenv*/
-// npm i dotenv
-require('dotenv').config()
+require('dotenv').config() // npm i dotenv
+
 const user = process.env.USERNAME
 const password = process.env.PASSWORD
+const fecha = new Date()
+const dia = `${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}`
+const hora = `${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`
 
-
-const generarMarcacion = async () => {
+const generarMarcacion = async (tipoMarcacion, nombreMarcacion) => {
     try {
         //? 1- Abrir chrome
         const browser = await puppeteer.launch({ headless: false /*, devtools: true*/ }) // headless: false para que habra el navegador
@@ -21,29 +22,32 @@ const generarMarcacion = async () => {
         await page.goto('https://intranet4.colegium.com/login')
 
         //? 2- Inicio sesion
-
+        console.log(`\nDia ${dia}.`)
+        console.log(`Iniciando sesion Intranet4 - ${user}`)
         await page.waitForSelector('#input-14', {visible: true})
         await page.type('#input-14', user, {delay:300})
         await page.type('#input-17', password, {delay:300})
         await page.click('#app > div > div > div > div:nth-child(2) > form > div.container > div.sn-login__actions.my-0 > button')
 
         //? 3- Genero marcación
+        console.log(`Generando ${tipoMarcacion} marcacion - ${nombreMarcacion}: ${hora} hrs.`)
         await page.waitForTimeout(2000)
         await page.click('body')
         await page.waitForTimeout(500)
         await page.waitForSelector('button[title="Marcaciones"]', {visible: true})
         await page.click('button[title="Marcaciones"]')
-        //! BOTON MARCACION?
-        // await page.click('#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > div > button')
+        // Boton marcacion
+        await page.waitForSelector('#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > div > button', {visible: true})
+        await page.click('#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > div > button')
 
         //? 4- En 15 seg termino proceso
-        await page.waitForTimeout(15000)
-        await process.exit()
-
-
+        await page.waitForTimeout(5000)
+        console.log('Proceso terminado.')
+        await browser.close();
     } catch (error) {
-        console.log('Error al iniciar marcaCron: ', error)
+        console.error('Error al iniciar marcaCron: ', error)
     }
 }
-// generarMarcacion()
+
+
 exports.generarMarcacion = generarMarcacion
