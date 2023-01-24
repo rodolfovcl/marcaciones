26698e38e59243fadb58523e8bce80f62d3e7c54
rodolfovenegas <rodolfovenegas.es@gmail.com>
2023-01-24 07:46:47 -0300

    Modifico hora cambio de dia

--- cron.js
+++ cron.js
@@ -4,7 +4,7 @@ const cron = require('node-cron')
 
 
 /*Desde las 07:58 todos los días de la semana de lunes a viernes*/
-cron.schedule('58 07 * * 1-5 ', () =>{ marcaciones.cambioDiaMarcacion() }) /*Cambio de dia*/
+cron.schedule('59 07 * * 1-5 ', () =>{ marcaciones.cambioDiaMarcacion() }) /*Cambio de dia*/
 cron.schedule('00 08 * * 1-5 ', () =>{ marcaciones.generarMarcacion('1RA','Inicio de jornada') })
 cron.schedule('00 13 * * 1-5 ', () =>{ marcaciones.generarMarcacion('2DA','Salida al almuerzo') })
 cron.schedule('00 14 * * 1-5 ', () =>{ marcaciones.generarMarcacion('3RA','Vuelta del almuerzo') })
